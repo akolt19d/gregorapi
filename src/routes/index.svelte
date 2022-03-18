40d4a80ddd1f2c736737efa8bd1a75b8d84c4ea6
akolt19d <90689597+akolt19d@users.noreply.github.com>
2022-03-18 15:44:54 +0100

    sadas
    dsadasd

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+    const handleClick = () => {
+        console.log("omamale")
+    }
+</script>
+
+<div class="flex justify-center items-center h-screen flex-col">
+    <h1 class="text-4xl">Eluwina</h1>
+    <button on:click={handleClick} class="bg-slate-600 px-5 py-2 my-3 border-4 border-slate-700 text-white hover:bg-slate-800 hover:border-slate-900 rounded-md">Zaloguj się</button>
+</div>
