b8d37672621c031b9ff101a32c9bc43e900dad66
akolt19d <90689597+akolt19d@users.noreply.github.com>
2022-03-21 15:45:02 +0100

    login
    essior

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,12 @@
 <script>
     import {fade} from 'svelte/transition'
+
+    const handleClick = () => {
+        window.location.replace('/login')
+    }
 </script>
 
 <div class="flex justify-center items-center h-screen flex-col" transition:fade>
     <h1 class="text-4xl">Eluwina</h1>
-    <button class="bg-slate-600 px-5 py-2 my-3 border-4 border-slate-700 text-white hover:bg-slate-800 hover:border-slate-900 rounded-md">Zaloguj się</button>
+    <button on:click={handleClick} class="bg-slate-600 px-5 py-2 my-3 border-4 border-slate-700 text-white hover:bg-slate-800 hover:border-slate-900 rounded-md">Zaloguj się</button>
 </div>
--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -5,6 +5,9 @@
 
 </script>
 
-<div transition:fade>
-    oooomamale
+<div transition:fade class="flex justify-center items-center h-screen flex-col">
+    <section class="bg-slate-600 h-60 w-60">
+        <input type="text" placeholder="Nazwa użytkownika">
+        <input type="password" placeholder="Hasło">
+    </section>
 </div>
